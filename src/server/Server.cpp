a18a9cd0150e246d2603123a5828d3860ca20065
Albina Peposhi <codegirl.al@gmail.com>
2024-09-14 14:14:56 +0200

    Added server basic functions

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 17:12:41 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/09/12 17:14:59 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/09/14 12:50:19 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,34 @@ Server &Server::operator=(const Server &other)
 {
 	(void)other;
 	return (*this);
+}
+
+void Server::start()
+{
+	std::cout << "Server started" << std::endl;
+}
+
+void Server::stop()
+{
+	std::cout << "Server stopped" << std::endl;
+}
+
+void Server::restart()
+{
+	std::cout << "Server restarted" << std::endl;
+}
+
+void Server::reload()
+{
+	std::cout << "Server reloaded" << std::endl;
+}
+
+void Server::status()
+{
+	std::cout << "Server status" << std::endl;
+}
+
+void Server::help()
+{
+	std::cout << "Server help" << std::endl;
 }
