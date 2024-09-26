8783f0c6b734ebaf08684a6d5a501a295ebb4a38
Albina Peposhi <codegirl.al@gmail.com>
2024-09-26 22:14:05 +0200

    Baseline for the main functionality

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 17:15:50 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/09/12 17:16:07 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/09/26 20:12:13 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,15 @@ int main()
 	Client client;
 	Server server;
 
-	(void)client;
-	(void)server;
+	try {
+		// start signals
+		// SIGINT
+		// SIGQUIT
+		server.init();
+	}
+	catch (std::exception &e) {
+		server.fdCloser();
+		std::cerr << e.what() << std::endl;
+	}
 	return (0);
 }
