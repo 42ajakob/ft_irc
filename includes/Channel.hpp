65f32f2dc86007a611a1fb7f66357f9f3bf84395
JFikents <138692812+JorFik@users.noreply.github.com>
2024-10-17 13:55:30 +0200

    Added Mode::Count to Channel

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 21:45:05 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/10/08 21:37:48 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/10/17 13:54:03 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,6 @@ class Client;
 
 class Channel
 {
-	private:
-		static std::vector<std::string>	_usedNames;
-		std::string			_name;
-		std::vector<Client>	_members;
-		std::vector<Client>	_operators;
-		std::string			_topic;
-		std::bitset<4>		_mode;
-		std::string			_password;
-		uint16_t			_userLimit;
-		
-		bool	isNameAvailable(std::string name);
 	public:
 		Channel()									= delete;
 		Channel(const Channel &other)				= delete;
@@ -40,22 +29,35 @@ class Channel
 		Channel(std::string name,const Client &creator);
 		~Channel();
 
-		enum class Mode;
-
 		void join(Client &client);
 		void kick(const std::string &nickname);
 		void invite(const std::string &nickname);
 		void leave(Client &client);
 		void mode(Client &client);
 		void clear();
+
+	enum class Mode
+	{
+		InviteOnly,
+		ProtectedTopic,
+		PasswordProtected,
+		UserLimit,
+		Count
+	};
+
+	private:
+		static std::vector<std::string>	_usedNames;
+
+		std::bitset<static_cast<size_t>(Mode::Count)>	_mode;
+		std::string			_name;
+		std::vector<Client>	_members;
+		std::vector<Client>	_operators;
+		std::string			_topic;
+		std::string			_password;
+		uint16_t			_userLimit;
+
+		bool	isNameAvailable(std::string name);
 };
 
-enum class Channel::Mode
-{
-	InviteOnly,
-	ProtectedTopic,
-	PasswordProtected,
-	UserLimit
-};
 
 #endif
