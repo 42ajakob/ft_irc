3622c0bffcdd8ad1bde8309196ab6adf2f9611be
ajakob <ajakob@student.42heilbronn.de>
2024-10-18 19:44:07 +0200

    changed hostName to ipAddr && added lastMsg && create setters/getters

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Client.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
+/*   By: ajakob <ajakob@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 17:04:49 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/10/17 17:00:55 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/10/18 19:33:52 by ajakob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,10 @@ class Client
 private:
 	static std::vector<std::string>	_usedNicknames;
 	int			_fd;
-	std::string	_nickname;
-	std::string	_hostName;
+	std::string	_nickName;
+	std::string	_ipAddr;
 	std::string	_userName;
+	std::string _lastMsg;
 
 	bool	isNicknameAvailable(std::string nickname);
 
@@ -34,12 +35,17 @@ public:
 	~Client();
 
 	void				setFd(int fd_value);
-	void				setNickname(std::string nickname);
+	void				setNickName(std::string nickname);
+	void				setIpAddr(std::string ipAddr);
 	void				setUserName(std::string username);
+	void				setLastMsg(std::string buffer);
+
 	int					getFd() const;
 	const std::string	&getNickname() const;
+	const std::string	&getIpAddr() const;
+	const std::string	&getLastMsg() const;
 
-	bool operator ==(const Client &other) const;
+	bool operator==(const Client &other) const;
 };
 
 #endif
