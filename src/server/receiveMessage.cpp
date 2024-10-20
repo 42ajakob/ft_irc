52abeecd5f6c49a9c3099329688928aaaba1399e
JFikents <138692812+JorFik@users.noreply.github.com>
2024-10-20 20:42:37 +0200

    Fixing log order

--- src/server/receiveMessage.cpp
+++ src/server/receiveMessage.cpp
@@ -6,7 +6,7 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 13:08:37 by JFikents          #+#    #+#             */
-/*   Updated: 2024/10/20 19:02:51 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/10/20 19:52:08 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ void	debugBypass(std::string &line)
 	line += "\r\n";
 	try{
 		write(std::stoi(line.c_str() + 7), &line[9], line.size() - 8);
-		std::cout << "Message sent to client " << std::stoi(line.c_str() + 7) << ": " << &line[8] << std::flush;
+		std::cout << "Message sent to client " << std::stoi(line.c_str() + 7) << ": " << &line[9] << std::flush;
 	} catch (const std::exception &e) {
 		std::cerr << "Error bypassing :" << e.what() << std::endl;
 	}
@@ -64,6 +64,7 @@ void	executeCommand(eCommand command, std::string &line, pollfd &pollFD)
 	{
 		case eCommand::PING:
 			send(pollFD.fd, "PONG\n", 5, 0);
+			std::cout << "PONG sent to client " << pollFD.fd << std::endl;
 			break;
 		case eCommand::PRIVMSG:
 			break;
@@ -107,10 +108,10 @@ void	Server::receiveMessage(pollfd &pollFD)
 	while (line.size() > 0)
 	{
 		command = checkForCommand(line);
-		if (command != eCommand::UNKNOWN)
-			executeCommand(command, line, pollFD);
 		if (command != eCommand::DEBUG_BYPASS)
 			std::cout << "Received message from client " << pollFD.fd << ": " << line << std::endl;
+		if (command != eCommand::UNKNOWN)
+			executeCommand(command, line, pollFD);
 		std::getline(ss, line, '\n');
 	}
 	std::memset(buffer, 0, sizeof(buffer));
