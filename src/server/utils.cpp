de411c1040ff1624711c9311b25cbf473dc89bc6
JFikents <138692812+JorFik@users.noreply.github.com>
2024-10-28 18:26:18 +0100

    Fixing findNextParameter

--- src/server/utils.cpp
+++ src/server/utils.cpp
@@ -6,50 +6,35 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 20:27:36 by JFikents          #+#    #+#             */
-/*   Updated: 2024/10/26 20:56:21 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/10/28 17:53:07 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
-static std::string::iterator	jumpSpaces(std::string::iterator it, std::string::iterator end)
-{
-	while (it != end && *it == ' ')
-		it++;
-	return (it);
-}
-
-size_t	Server::findNextParameter(std::string &line, size_t pos = 0)
+static size_t	jumpSpaces(const std::string &line, size_t pos)
 {
 	auto it = line.begin() + pos;
 	auto end = line.end();
 
-	it = jumpSpaces(it, end);
-	if (it == end)
-		return (std::string::npos);
-	auto next_space = line.find(' ', pos);
-	auto next_colon = line.find(':', pos);
+	while (it != end && *it == ' ')
+		++it;
+	return (it - line.begin());
+}
 
-	if (next_space == std::string::npos && next_colon == std::string::npos)
+size_t	findNextParameter(const std::string &line, size_t pos)
+{
+	pos = jumpSpaces(line, pos);
+	size_t next_space = line.find(' ', pos);
+	size_t next_colon = line.find(':', pos);
+
+	if (line[pos] == ':'
+		|| (next_space == std::string::npos && next_colon == std::string::npos))
 		return (std::string::npos);
 	if (next_space == std::string::npos)
 		return (next_colon);
+	next_space = jumpSpaces(line, next_space);
 	if (next_colon == std::string::npos || next_space < next_colon)
 		return (next_space);
 	return (next_colon);
 }
-
-// size_t	Server::findNextParameter(std::string &line)
-// {
-// 	auto next_space = line.find(' ');
-// 	auto next_colon = line.find(':');
-// 	auto end = line.end();
-
-// 	if (next_space == end && next_colon == end)
-// 		return (end);
-// 	if (next_space == end)
-// 		return (next_colon);
-// 	if (next_colon == end || next_space < next_colon)
-// 		return (next_space);
-// 	return (next_colon);
-// }
