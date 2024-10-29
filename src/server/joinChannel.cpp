389e4fb08c6e05f0b8646789e7eda04374688979
JFikents <138692812+JorFik@users.noreply.github.com>
2024-10-29 18:02:21 +0100

    Fix join parsing && password capability

--- src/server/joinChannel.cpp
+++ src/server/joinChannel.cpp
@@ -6,7 +6,7 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 20:09:21 by JFikents          #+#    #+#             */
-/*   Updated: 2024/10/29 16:54:45 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/10/29 17:28:32 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,20 +33,28 @@ std::vector<string>	split(const string &line, const char &delimiter)
 void	Server::joinChannel(const int &fd, string &line)
 {
 	size_t					pos = findNextParameter(line);
+	string					rawChannelNames;
 	std::vector<string>		channelNames;
 	std::vector<string>		passwords;
 
 	if (pos == string::npos)
 		throw std::runtime_error("JOIN command without channel name");
-	channelNames = split(line.substr(pos), ',');
-	if ((pos = findNextParameter(line, pos)) != string::npos)
-		passwords = split(line.substr(pos), ',');
+	rawChannelNames = line.substr(pos);
+	if ((pos = findNextParameter(rawChannelNames)) != string::npos)
+	{
+		passwords = split(rawChannelNames.substr(pos), ',');
+		rawChannelNames.erase(pos - 1);
+	}
+	channelNames = split(rawChannelNames, ',');
 	while (channelNames.size() > passwords.size())
 		passwords.emplace_back("");
-	for (string ChannelName : channelNames)
+	for (size_t i = 0; i < channelNames.size(); i++)
 	{
+		string	ChannelName = channelNames[i];
+		string	Password = passwords[i];
 		toLower(ChannelName);
 		// TODO: Decide on Channel creation method
 		std::cout << "Client " << _clients[fd].getNickname() << " joined channel " << ChannelName << std::endl;
+		std::cout << "Password: <" << Password + '>' << std::endl;
 	}
 }
