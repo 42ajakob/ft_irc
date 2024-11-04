be56f7489c2666b98a71ae78b01b4d90e51d520e
Albina Peposhi <codegirl.al@gmail.com>
2024-11-04 14:44:23 +0100

    WIP: Join command, redone

--- src/commands/Join.cpp
+++ src/commands/Join.cpp
@@ -6,27 +6,20 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 13:19:30 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/10/14 15:57:30 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/11/04 14:42:46 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Channel.hpp"
 
-void Channel::join(Client &client)
+void Channel::join(Client &client, const string &password)
 {
-	if (std::find(this->clients.begin(), this->clients.end(), client) != this->clients.end())
-	{
-		client.send(":%s 341 %s %s :You are already in this channel", SERVER_NAME, client.getNick().c_str(), this->name.c_str());
-		return ;
-	}
-	if (this->inviteOnly)
-	{
-		if (std::find(this->invited.begin(), this->invited.end(), client.getNick()) == this->invited.end())
-		{
-			client.send(":%s 341 %s %s :You are not invited to this channel", SERVER_NAME, client.getNick().c_str(), this->name.c_str());
-			return ;
-		}
-	}
-	this->clients.push_back(client);
-	client.send(":%s 341 %s %s :You have joined channel %s", SERVER_NAME, client.getNick().c_str(), this->name.c_str(), this->name.c_str());
+	if (_password != password)
+		throw std::runtime_error("Invalid password");
+
+	if (_members.find(&client) != _members.end())
+		throw std::runtime_error("Already in channel");
+
+	_members.insert(&client);
+	client.addToSendBuffer("JOIN " + _name + "\r\n");
 }
