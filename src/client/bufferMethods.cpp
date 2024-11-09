fd7e71e30371dc684177f14c7db7fa61edadcabb
Albina Peposhi <codegirl.al@gmail.com>
2024-11-09 18:22:59 +0100

    Add noexcept specifier to buffer methods in Client class

--- src/client/bufferMethods.cpp
+++ src/client/bufferMethods.cpp
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   bufferMethods.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
+/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 17:40:13 by JFikents          #+#    #+#             */
-/*   Updated: 2024/10/22 20:58:15 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/11/09 17:44:42 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Client.hpp"
 
-void	Client::addToSendBuffer(string buffer)
+void	Client::addToSendBuffer(string buffer) noexcept
 {
 	_sendBuffer += buffer;
 }
 
-void	Client::addToRecvBuffer(string buffer)
+void	Client::addToRecvBuffer(string buffer) noexcept
 {
 	_recvBuffer += buffer;
 }
