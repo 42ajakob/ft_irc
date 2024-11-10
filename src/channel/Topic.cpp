5a188ec512abe1f6578d1c5c96416b90f033c254
Albina Peposhi <codegirl.al@gmail.com>
2024-11-10 16:01:24 +0100

    Handled topic on the channel side

--- src/channel/Topic.cpp
+++ src/channel/Topic.cpp
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:58:23 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/11/10 15:39:42 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/11/10 16:00:48 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 
 void Channel::topic(const string &topic, const Client &client)
 {
-	_topic = topic;
-	sendChannelInfo(client);
+	try {
+		if (_members.find(&client) == _members.end())
+			throw std::invalid_argument("You're not on that channel");
+		if (_operators.find(&client) == _operators.end() && _mode.test(static_cast<size_t>(Mode::TopicProtected)))
+			throw std::invalid_argument("Cannot change topic");
+		_topic = topic;
+	}
+	catch (const std::exception &e)
+	{
+		string problem(e.what());
+
+		if (problem == "You're not on that channel")
+			client.addToSendBuffer(ERR_NOTONCHANNEL(_name));
+		else if (problem == "Cannot change topic")
+			client.addToSendBuffer(ERR_CHANOPRIVSNEEDED(_name));
+		else
+			client.addToSendBuffer(problem);
+		std::cerr << "Error changing topic: " << problem << std::endl;
+	}
 }
