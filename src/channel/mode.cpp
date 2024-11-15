315f0f4cc6c3b0db31b7111e27d3d86b5bcddd61
42ajakob <ajakob@student.42heilbronn.de>
2024-11-15 16:39:32 +0100

    silent fail for invalid limit

--- src/channel/mode.cpp
+++ src/channel/mode.cpp
@@ -6,7 +6,7 @@
 /*   By: ajakob <ajakob@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 18:00:07 by JFikents          #+#    #+#             */
-/*   Updated: 2024/11/15 16:12:38 by ajakob           ###   ########.fr       */
+/*   Updated: 2024/11/15 16:38:43 by ajakob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,17 +35,19 @@ void	Channel::_demoteClientFromOperator(const string &origin, Client &client)
 
 void Channel::mode(const string &mode, Client &client, const string &mode_param)
 {
-	std::cout << "Mode " << mode << " Client " << client.getNickname() << " modeparam " << mode_param << std::endl;
-	if (mode.size() >= 2 && mode[1] == 'o')
+	
+	if (mode_param.empty())
+		throw std::invalid_argument(ERR_NEEDMOREPARAMS(client.getNickname(), "MODE"));
+	else if (mode.size() >= 2 && mode[1] == 'o')
 	{
 		Client &target = Server::getInstance().getClientByNickname(mode_param);
 		
 		if (_members.find(&target) == _members.end())
-		throw std::invalid_argument(ERR_NOTONCHANNEL(_name));
-			else if (_operators.find(&client) == _operators.end())
-		throw std::invalid_argument(ERR_CHANOPRIVSNEEDED(_name));
-			else if (_members.find(&target) == _members.end())
-		throw std::invalid_argument(ERR_USERNOTINCHANNEL(target.getNickname(), _name));
+			throw std::invalid_argument(ERR_NOTONCHANNEL(_name));
+		else if (_operators.find(&client) == _operators.end())
+			throw std::invalid_argument(ERR_CHANOPRIVSNEEDED(_name));
+		else if (_members.find(&target) == _members.end())
+			throw std::invalid_argument(ERR_USERNOTINCHANNEL(target.getNickname(), _name));
 
 		if (_operators.find(&target) == _operators.end()
 			&& mode == "+o" && client.getNickname() != target.getNickname())
@@ -66,7 +68,11 @@ void Channel::mode(const string &mode, Client &client, const string &mode_param)
 	}
 	else if (mode == "+l")
 	{
-		_userLimit = std::stoi(mode_param);
+		int newLimit = std::stoi(mode_param);
+		
+		if (newLimit < 1)
+			return ;
+		_userLimit = newLimit;
 		_mode.set(UserLimit);
 	}
 	else if (mode == "-l")
