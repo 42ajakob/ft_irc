a636626f8c4c36f01f7f7a81f6ae255db4fb8129
42ajakob <ajakob@student.42heilbronn.de>
2024-11-15 15:06:46 +0100

    Fixing if trying to invite multiple times

--- src/channel/invite.cpp
+++ src/channel/invite.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   invite.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ajakob <ajakob@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:16:37 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/11/09 18:17:17 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/11/15 14:50:19 by ajakob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,18 @@
 void Channel::invite(const string &nickname, Client &client)
 {
 	try {
+		auto target = &Server::getInstance().getClientByNickname(nickname);
+		
 		if (_members.find(&client) == _members.end())
 			throw std::invalid_argument("You're not on that channel");
 		if (_operators.find(&client) == _operators.end())
 			throw std::invalid_argument("Cannot send invites");
-		if (_members.find(&Server::getInstance().getClientByNickname(nickname)) != _members.end())
+		if (_members.find(target) != _members.end())
 			throw std::invalid_argument("They are already on that channel");
-		_invited.insert(&Server::getInstance().getClientByNickname(nickname));
+		if (_invited.find(target) != _invited.end())
+			throw std::invalid_argument("You're already invited");
+		_invited.insert(target);
+		target->addToSendBuffer(RPL_INVITING(_name, client.getNickname()));
 	}
 	catch (const std::exception &e)
 	{
@@ -37,8 +42,6 @@ void Channel::invite(const string &nickname, Client &client)
 			client.addToSendBuffer(ERR_CHANOPRIVSNEEDED(_name));
 		else if (problem == "They are already on that channel")
 			client.addToSendBuffer(ERR_USERONCHANNEL(client.getNickname(), nickname, _name));
-		else
-			client.addToSendBuffer(problem);
 		std::cerr << "Error inviting user: " << problem << std::endl;
 	}
 }
